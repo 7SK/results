0ecea4377bcbfde1ecfd6ee8a7e0a789233dc8ec
Sandeep Kumar <Sandeep.Kumar@amd.com>
2018-12-20 09:20:45 +0000

    add score for 8xMI25

--- v0.5.0/google/cloud_v100x8/code/resnet/run_and_time_8xMI.sh
+++ v0.5.0/google/cloud_v100x8/code/resnet/run_and_time_8xMI.sh
@@ -5,7 +5,7 @@ set -o pipefail
 MLP_HOST_OUTPUT_DIR=`pwd`/output
 sudo mkdir -p $MLP_HOST_OUTPUT_DIR
 
-sudo docker build . -f Dockerfile.rocm -t rocm-image-classification
-sudo docker run -v /data:/data \
+sudo docker build . -f Dockerfile.rocm -t rocm
+sudo docker run -v $MLP_HOST_DATA_DIR:/data \
 -v $MLP_HOST_OUTPUT_DIR:/output -v /proc:/host_proc --device=/dev/kfd/ --device=/dev/dri/ \
--t rocm-image-classification:latest /root/run_helper_8xMI.sh 2>&1 | sudo tee output.txt
+-t rocm:latest /root/run_helper_8xMI.sh 2>&1 | sudo tee output.txt
